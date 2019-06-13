72729229347f5c461988ffc9c4fa44ab0f85a141
Arshad Mehmood <arshad115@hotmail.com>
2019-06-13 15:30:42 +0200

    improve card

--- crosspromote/src/main/java/com/kookydroidapps/adapters/AppsAdapter.java
+++ crosspromote/src/main/java/com/kookydroidapps/adapters/AppsAdapter.java
@@ -90,7 +90,7 @@ public class AppsAdapter extends RecyclerView.Adapter<AppsAdapter.ViewHolder> {
                 try {
                     context.startActivity(intent);
                 } catch (Exception e) {
-                    Toast.makeText(context, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "Error: " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
                 }
             }
